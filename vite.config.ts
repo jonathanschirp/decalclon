1fdce6b9d3c31c06a43b7749687c5fb0ef9211d9
Jonathan Schirp <jonathan.schirp@web.de>
2026-04-23 23:45:19 +0200

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,18 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rolldownOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/firebase') || id.includes('node_modules/@firebase')) {
+            return 'firebase';
+          }
+          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom') || id.includes('node_modules/react-router')) {
+            return 'react';
+          }
+        },
+      },
+    },
+  },
 })
